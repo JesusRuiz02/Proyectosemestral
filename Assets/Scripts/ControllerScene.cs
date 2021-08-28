75de26baffbff0f9dd59d66348aba1ad7b3b33c6
JesusRuiz02 <zap295@amerike.edu.mx>
2021-08-28 16:57:16 -0600

    Actualizacion 28 de Agosto

--- Assets/Scripts/ControllerScene.cs
+++ Assets/Scripts/ControllerScene.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class ControllerEscena : MonoBehaviour
+public class ControllerScene : MonoBehaviour
 {
+   
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
--- Assets/Scripts/MovementShip.cs
+++ Assets/Scripts/MovementShip.cs
@@ -3,28 +3,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class MovementShip : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    private Rigidbody2D rb;
-    private float dirX;
-    private float MoveSpeed = 20f;
+    private Rigidbody2D _rb;
+    private float _dirX;
+    private const float  MoveSpeed= 20f;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        dirX = Input.acceleration.x * MoveSpeed;
+        _dirX = Input.acceleration.x * MoveSpeed;
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y);
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() 
     {
-        rb.velocity = new Vector2(dirX, 0f);
+        _rb.velocity = new Vector2(_dirX, 0f);
     }
+
+    public class PlayerCollision : MonoBehaviour
+    {
+        private int life = 3;
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+           ProcessCollision(collision.gameObject);
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            ProcessCollision(collision.gameObject);
+        }
+        private void ProcessCollision(GameObject collider)
+        {
+            if (collider.CompareTag("Obstacle"))
+            {
+                LostLife();
+            }
+
+            if (collider.CompareTag("Points"))
+            {
+                scorepoint();
+            }
+                
+        }
+        private void LostLife()
+        {
+            
+            Debug.Log("Lost a life");
+            life = life - 1;
+            if (life<=0)
+            {
+                Debug.Log("Perdiste");
+            }
+        }
+
+        private void scorepoint()
+        {
+            Debug.Log("You score a point");
+        }
+    }
+    
+
+   
 }
