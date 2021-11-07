ea9df08c1c069220cee2195000aa6f5fa5538fdc
JesusRuiz02 <zap295@amerike.edu.mx>
2021-11-06 23:32:43 -0600

    aa

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;using UnityEditor.VersionControl;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
--- Assets/Scripts/MainSettings.cs
+++ Assets/Scripts/MainSettings.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using UnityEditor.VersionControl;
+using UnityEditor;
 using UnityEngine;
 
 public class MainSettings : MonoBehaviour
